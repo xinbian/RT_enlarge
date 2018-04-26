25ebbde584713f8bc2ca503983f9a58b5f6053f1
Xin Bian <xinbian@users.noreply.github.com>
2018-04-26 10:51:09 -0400

    add one loop

--- cpH5.bash
+++ cpH5.bash
@@ -2,7 +2,7 @@
 #the application:1 HDF5 file run out of digits 2 want to change the domain ratio
 
 
-module load hdf5
+#!/bin/bash
 
 inFile="tests_single_new.h5"
 outFile="temp.h5"
@@ -14,3 +14,22 @@ h5copy -p -i $inFile  -o $outFile -s /Fields/Prho/$inTime -d /Fields/Prho/$outTi
 h5copy -p -i $inFile  -o $outFile -s /Fields/PPress/$inTime -d /Fields/PPress/$outTime
 h5copy -p -i $inFile  -o $outFile -s /Fields/PVy/$inTime -d /Fields/PVy/$outTime
 h5copy -p -i $inFile  -o $outFile -s /Fields/PVz/$inTime -d /Fields/PVz/$outTime
+
+
+
+#this part just copying to another file, keeping same time stamp
+#the apploication: hdf5 file is too large, extract some for post processing
+
+inFile="tests_single_new.h5"
+outFile="temp.h5"
+
+for step in '020000' '030000' '040000' '050000'; do
+	
+	for var in 'Prho' 'PVx' 'PVy' 'PVz' 'PPress'; do 
+
+		h5copy -p -i $inFile -o $outFile -s /Fields/$var/$step -d /Fields/$var/$step
+
+		echo "copying $var variable, $step step"
+
+	done
+
